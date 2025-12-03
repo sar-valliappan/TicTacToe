58e7d1a0d111149f4b20a6abe1c03f85db4a009d
sar-valliappan <173072045+sar-valliappan@users.noreply.github.com>
2025-12-02 22:34:47 -0800

    Minimax Logic

--- main.cpp
+++ main.cpp
@@ -15,36 +15,82 @@ void print_board(char board[3][3]) {
 bool check_win(char board[3][3], char player) {
     for (int i = 0; i < 3; i++) {
         if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
-            std::cout << "Player " << player << " wins!\n";
             return true;
         }
         if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
-            std::cout << "Player " << player << " wins!\n";
             return true;
         }
     }
     if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
-        std::cout << "Player " << player << " wins!\n";
         return true;
     }
     if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
-        std::cout << "Player " << player << " wins!\n";
         return true;
     }
     return false;
 }
 
-void computer_move(char board[3][3]) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 2);
-    int row = dis(gen);
-    int col = dis(gen);
-    while (board[row][col] != ' ') {
-        row = dis(gen);
-        col = dis(gen);
+bool is_tied(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') return false;
+        }
     }
-    board[row][col] = 'O';
+    return true;
+}
+
+int minimax(char board[3][3], bool isMaximizing) {
+    if (check_win(board, 'X')) return -10;
+    if (check_win(board, 'O')) return 10;
+    if (is_tied(board)) return 0;
+
+    if (isMaximizing) {
+        int best = std::numeric_limits<int>::min();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = 'O';
+                    best = std::max(best, minimax(board, false));
+                    board[i][j] = ' ';
+                }
+            }
+        }
+        return best;
+    } else {
+        int best = std::numeric_limits<int>::max();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = 'X';
+                    best = std::min(best, minimax(board, true));
+                    board[i][j] = ' ';
+                }
+            }
+        }
+        return best;
+    }
+}
+
+void computer_move(char board[3][3]) {
+    int bestVal = std::numeric_limits<int>::min();
+    int bestRow = -1;
+    int bestCol = -1;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] == ' ') {
+                board[i][j] = 'O';
+                int moveVal = minimax(board, false);
+                board[i][j] = ' ';
+
+                if (moveVal > bestVal) {
+                    bestRow = i;
+                    bestCol = j;
+                    bestVal = moveVal;
+                }
+            }
+
+    board[bestRow][bestCol] = 'O';
 }
 
 void game(char board[3][3], char choice) {
@@ -67,6 +113,11 @@ void game(char board[3][3], char choice) {
         board[row][col] = player;
         print_board(board);
         if (check_win(board, player)) {
+            std::cout << "Player " << player << " wins!\n";
+            break;
+        }
+        if (is_tied(board)) {
+            std::cout << "It's a tie!\n";
             break;
         }
         if (choice == 'n') {
@@ -82,10 +133,15 @@ void game(char board[3][3], char choice) {
         if (choice == 'y') {
             computer_move(board);
             std::cout << "Computer's move:\n";
+            print_board(board);
             if (check_win(board, 'O')) {
+                std::cout << "The computer wins!\n";
                 break;
             }
-            print_board(board);
+            if (is_tied(board)) {
+                std::cout << "It's a tie!\n";
+                break;
+            }
         }
     }
 }
